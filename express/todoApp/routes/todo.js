ed8d9015cb19e82f5f4c5cba2c654ff3bf109ca4
bezalel-7 <varadudi18@gmail.com>
2023-11-03 00:16:09 +0530

    Add CRUD functionality to todo API

--- express/todoApp/routes/todo.js
+++ express/todoApp/routes/todo.js
@@ -2,9 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 const { createTodo } = require('../controllers/createTodo')
-
+const { fetchAllTodos } = require('../controllers/fetchAllTodos')
+const { fetchOneid } = require('../controllers/fetchOneId')
+const { updateTodo } = require('../controllers/updateTodo')
+const { deleteTodo } = require('../controllers/deleteTodo')
 //define api routes
 
 router.post('/createTodo', createTodo)
+router.get('/fetchAllTodos', fetchAllTodos)
+router.get('/getById/:id', fetchOneid)
+router.put('/updateTodo/:id', updateTodo)
+router.delete('/deleteTodo/:id', deleteTodo)
 
 module.exports = router
